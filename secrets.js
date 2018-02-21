a2082f64319f1e94cbb579cc9855dc6fcb2ea024
Daniel Marques <dkdaniz@outlook.com>
2018-02-21 17:57:42 -0300

    alter secrects with my alterations

--- secrets.js
+++ secrets.js
@@ -1,48 +1,73 @@
-'use strict';
+ /*
+    ---------------- EXPLICAÇÃO ----------------
+    Você pode tanto adicionar uma nova posição quando adicionar um novo item para uma posição examplo:
+    1 - Adicionando um novo item 209 na posição 1  ['', '02', '205'],
+    resultado final: 
+      ['', '12', '203', '209'],
+      
+    2 - Adicionando uma nova posição, tem aquela hipotese que pode ser que o addr esteja escrito com dois d,
+    para testar essa hipotese voce adiciona abaixo do   ['d', 'D'],  ['','d', 'D'],  
+    resultado final: 
+     ['d', 'D'],
+     ['','d', 'D'], 
+     ['r', 'R'],
+     *importante, eu adicionei o item '' na posição 2 pois eu quero testar combinações tanto com duas letras d, e também somente com uma.
 
-var combinations = require('./combinations');
+     ** Esses dois exemplos eu copiei só parte das posicoes para não ficar grande o exemplo. 
 
-module.exports = (function() {
-
-    var secrets = [],
-        prefixedSecrets = [],
-        suffixedSecrets = [],
-        paddedSecrets = [],
-        passwords = [
-            'insecure',
-            'secret',
-        ],
-        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-        dividers = ['|', '&', '+', '-', '_', ' ', ''];
-
-
-    secrets = passwords;
-
-    secrets = secrets.concat(combinations(passwords, '!!'));
-
-    secrets = secrets.concat(combinations(dividers, passwords[0] + '!' + passwords[1]));
-    secrets = secrets.concat(combinations(dividers, passwords[1] + '!' + passwords[0]));
-
-    secrets.forEach(function(secret) {
-
-        prefixedSecrets = prefixedSecrets.concat(combinations(letters, '!!-' + secret));
-        prefixedSecrets = prefixedSecrets.concat(combinations(numbers, '!' + secret));
-        prefixedSecrets = prefixedSecrets.concat(combinations(numbers, '!-' + secret));
-        prefixedSecrets = prefixedSecrets.concat(combinations(numbers, '0!-' + secret));
-    });
-
-    secrets.forEach(function(secret) {
-
-        suffixedSecrets = suffixedSecrets.concat(combinations(numbers, secret + '-!'));
-        suffixedSecrets = suffixedSecrets.concat(combinations(numbers, secret + '-0!'));
-    });
-
-
-    secrets = secrets.concat(prefixedSecrets);
-    secrets = secrets.concat(suffixedSecrets);
-
-    return secrets;
-
-}());
+     3 - Caso deseja criar uma nova hipotese é possível.
+     Exemplo: Eu tenho uma senha que é 'casa' mas não sei como foi escrita 'cAsa','c@Sa'... 
+     resultado final: 
+     posicoes = [
+        ['c', 'C'],
+        ['a', 'A', '@'],
+        ['s', 'S'],
+        ['a', 'A', '@']        
+    ];
+     ---------------- ----------------- 
+     * Essa explicação  é um comentário do código e não será executada.
+     ** Quanto maior o número de posiçoes e itens, maior o número de combinações. 
+     *** Boa sorte e bora quebrar essa senha. 
+    */
+    'use strict';
+    
+    var combinations = require('./combinations');
+    
+    module.exports = (function () {
+    
+        var posicoes = [];
+        var senhas = [];
+       
+        posicoes = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ];
+    
+        combinations = function (pos, c, soFar, total) {
+            if (pos == c.length) {
+                // LEIA -> caso não deseja ver todas as possibilidades de senhas sendo imprimidas no console coloque um // antes do console.log(soFar)
+                console.log(soFar);
+                senhas = senhas.concat(soFar);
+                return;            
+            }
+            for (var i = 0; i != c[pos].length; i++) {
+                combinations(pos + 1, c, soFar + c[pos][i], total);
+            }
+        }
+    
+        combinations(0, posicoes, '', 0);
+    
+        return senhas;
+    
+    }());
+    
